1703b4b29de2def699aa58b636faaf0d16cd46a9
Katetown20 <https://github.com/Katetown20/test.git>
2023-03-28 14:30:59 +0300

    '28.03 commit'

--- env/shop/example/views.py
+++ env/shop/example/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -10,26 +12,65 @@ menu = [{'title': 'О сайте','url_name': 'about'},
         {'title': 'Обратная связь', 'url_name': 'contact'},
         {'title': 'Войти', 'url_name': 'login'}]
 
+class ExampleHome(ListView):
+    model = Example
+    template_name = 'example/index.html'
+    context_object_name = 'posts'
 
-def index(request):
-    posts = Example.objects.all()
-    cats = Category.objects.all()
-    context = {
-        'posts': posts,
-        'cats':cats,
-        'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
-    return render(request, 'example/index.html', context = context)
+#передача динамических и статических данных
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Example.objects.filter(is_published=True)
+
+# def index(request):
+#     posts = Example.objects.all()
+#
+#     context = {
+#         'posts': posts,
+#
+#         'menu': menu,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'example/index.html', context = context)
 
 
 def about(request):
     return render(request, 'example/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def addpage(request):
-    return HttpResponse('Добавление статьи')
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             try:
+#                 form.save()
+#                 return redirect('home')
+#             except:
+#                 form.add_error(None, 'Ошибка добавления поста')
+#
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'example/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'example/addpage.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
 
 
 def contact(request):
@@ -44,24 +85,61 @@ def pageNotFound(requesr, exception):
     return HttpResponseNotFound("<h1>The page you are looking for was not found</h1>")
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отобранные страницы с id = {post_id}')
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Example, slug=post_slug)
+#
+#     context = {
+#         'posts': post,
+#         'menu': menu,
+#         'title': 'post.title',
+#         'cat_selected': post.cat_id,
+#     }
+#     return render(request, 'example/post.html', context=context)
+
+class ShowPost(DetailView):
+    model = Example
+    template_name = 'example/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
 
 
-def show_category(request, cat_id):
-    posts = Example.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
+class ExampleCategory(ListView):
+    model = Example
+    template_name = 'example/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
 
-    if len(posts)  == 0:
-        raise Http404()
+    def get_queryset(self):
+        return Example.objects.filter(cat__slug=self.kwargs['cat_slug'],  is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Категория +' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
 
 
-    context = {
-        'posts': posts,
-        'cats': cats,
-        'menu': menu,
-        'title': 'Отображение категорий',
-        'cat_selected': cat_id,
-    }
-    return render(request, 'example/index.html', context=context)
+# def show_category(request, cat_id):
+#     posts = Example.objects.filter(cat_id=cat_id)
+#
+#
+#     if len(posts)  == 0:
+#         raise Http404()
+#
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Отображение категорий',
+#         'cat_selected':  cat_id,
+#     }
+#     return render(request, 'example/index.html', context=context)
